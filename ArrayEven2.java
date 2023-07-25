97c62a90149835bce6c2e295a2df020fe852f336
Madhu sudhan N <127393737+MadhuSudhanN@users.noreply.github.com>
2023-07-25 19:49:48 +0530

    main interchanged

--- ArrayEven2.java
+++ ArrayEven2.java
@@ -1,5 +1,12 @@
 class ArrayEven2{
-
+   public static void main(String[] args) {
+         System.out.println("main method started");
+         int[] a1 = {10,20,30,40,50};
+         disp(a1);
+         System.out.println("main methos ended");
+ 
+ 
+     }
     static void disp(int[] arr){
         System.out.println("Array size" +arr.length);
         //length is property of array ,which holds size of the Array
@@ -13,12 +20,5 @@ class ArrayEven2{
     }
      
     
-     public static void main(String[] args) {
-         System.out.println("main method started");
-         int[] a1 = {10,20,30,40,50};
-         disp(a1);
-         System.out.println("main methos ended");
- 
- 
-     }
+    
  }
